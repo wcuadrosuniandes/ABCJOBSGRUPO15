ee3a932a53874a54160207a69a01eb329739087f
Andres Soler <andres.soler@talent.com>
2023-09-04 14:50:13 -0500

    changing code complexity

--- experimento1/APIGateway/views/views.py
+++ experimento1/APIGateway/views/views.py
@@ -6,20 +6,20 @@ class RegisterCandidate(Resource):
     def get(self):
         try:
             response = requests.get('http://localhost:5000/register-candidate')
-            if response.status_code == 200:
-                return response.json(), 200
-            else:
+            if response.status_code != 200:
                 return {'message': 'Error on register API'}, response.status_code
+            
+            return response.json(), 200
         except Exception as e:
             return {'message': 'Error', 'error': e}, 500
     
     def post(self):
         try:
             response = requests.post('http://localhost:5000/register-candidate')
-            if response.status_code == 200:
-                return response.json(), 200
-            else:
+            if response.status_code != 200:
                 return {'message': 'Error on register API'}, response.status_code
+            
+            return response.json(), 200
         except Exception as e:
             return {'message': 'Error', 'error': e}, 500
     
