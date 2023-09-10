7c84839466e0659d184202a950e83a5629470a4a
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-09-10 12:15:21 -0500

    refactor: Reduce redeclaration of variables & handle JSON parse errors

--- experimento1/FormRegisterCandidateView/script.py
+++ experimento1/FormRegisterCandidateView/script.py
@@ -1,28 +1,32 @@
 import requests
 
+data = {
+    "nombre": "test",
+    "apellido": "test",
+    "correo": "test@test.com",
+    "experiencia": 4,
+    "titulo": "test",
+    "universidad": "test",
+}
+headers = {
+    "Content-Type": "application/json",
+}
+
 for i in range(0, 100):
-    data = {
-        "nombre": "test",
-        "apellido": "test",
-        "correo": "test@test.com",
-        "experiencia": 4,
-        "titulo": "test",
-        "universidad": "test",
-    }
+    response = requests.post(
+        "http://localhost:5000/register-candidate", json=data, headers=headers
+    )
 
-    headers = {
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.post("http://localhost:5000/register-candidate", json=data, headers=headers)
-    
     if response.status_code != 200:
-        print('Estamos teniendo problemas intenta mas tarde')
-    else:
-        correo = response.json().get('correo', '')
-        mensaje = response.json().get('mensaje', '')
-        status = response.json().get('status', '')
+        print(f"Estamos teniendo problemas intenta mas tarde - Status: {response.status_code}")
+        continue
 
-        output = f'Correo: {correo} - Mensaje: {mensaje} - Status: {status}'
+    try:
+        json = response.json()
+        correo = json.get("correo", "")
+        mensaje = json.get("mensaje", "")
+        status = json.get("status", "")
 
-        print(output)
+        print(f"Correo: {correo} - Mensaje: {mensaje} - Status: {status}")
+    except requests.exceptions.JSONDecodeError:
+        print("La respuesta no es un json válido")
