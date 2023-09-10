f555020e569a8025f10d951724d6e2a46b7134da
Juan José Montenegro P <jj.montenegro@uniandes.edu.co>
2023-09-10 12:49:22 -0500

    Refactor monitor.py

--- experimento1/APIGateway/monitor.py
+++ experimento1/APIGateway/monitor.py
@@ -1,5 +1,7 @@
+from flask import request
 from functools import wraps
 import datetime
+
 
 def monitor_registration_errors(func):
     @wraps(func)
@@ -7,16 +9,26 @@ def monitor_registration_errors(func):
         # Call the wrapped function
         message, response = func(*args, **kwargs)
 
+        if response.status_code == 200:
+            return message, response.status_code
+
         # Log errors if the response code is not 200
-        if response.status_code != 200:
-            # get the message of the response
-            error_response = response.json().get('error', '')
+        # get the message of the response
+        try:
+            json = response.json()
 
-            if message['error'] == 'Respuesta vacia del register API':
-                error_response = 'Respuesta vacia del register API'
+            error_response = json.get("error", "")
 
-            with open('error.log', 'a') as f:
-                f.write(f'{datetime.datetime.now()} - Error: {error_response}\n')
+            if message["error"] == "Respuesta vacia del register API":
+                error_response = "Respuesta vacia del register API"
+
+            with open("error.log", "a") as f:
+                f.write(f"{datetime.datetime.now()} - Error: {error_response}\n")
+        except request.exceptions.JSONDecodeError:
+            with open("error.log", "a") as f:
+                f.write(
+                    f"{datetime.datetime.now()} - Error: {response.text} - Could not decode json \n"
+                )
 
         return message, response.status_code
 
